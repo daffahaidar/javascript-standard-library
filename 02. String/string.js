001d1b9671faffce0a521ca1e59dd9f31a41e6bd
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-08-13 20:36:29 +0700

    String

---
+++ 02. String/string.js
@@ -0,0 +1,11 @@
+const person = "Daffa Haidar Nabil Zufar";
+
+console.info(person.length); // to get the length of the string
+console.info(person.toLowerCase()); // to get the string in lowercase
+console.info(person.toUpperCase()); // to get the string in uppercase
+console.info(person.split(" ")); // to get the string in array format
+console.info(person); // to get the string in original format
+
+const input = "     Daffa Haidar Nabil Zufar     ";
+console.info(input.trim()); // to get the string after trimming the spaces
+console.info(input);
