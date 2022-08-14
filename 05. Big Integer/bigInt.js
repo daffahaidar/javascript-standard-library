4f0faf557fb21631064d82d93a8451aafceb51a0
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-08-14 18:49:29 +0700

    Big Integer

---
+++ 05. Big Integer/bigInt.js
@@ -0,0 +1,8 @@
+// big integer is a big number library for javascript that can be used to perform mathematical operations on large numbers.
+
+const a = BigInt(Number.MAX_SAFE_INTEGER);
+const b = BigInt(Number.MAX_SAFE_INTEGER);
+
+const c = a + b;
+console.info(c);
+console.info(typeof c);
