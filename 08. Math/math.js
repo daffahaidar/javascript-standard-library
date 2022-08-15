6886af70d9118bb6d03bf93478ddf4113d9b8759
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-08-15 19:35:16 +0700

    Math

---
+++ 08. Math/math.js
@@ -0,0 +1,8 @@
+// math standard library is used to perform mathematical operations
+
+console.info(Math.max(10, 11, 12, 13, 8)); // to find the maximum value
+console.info(Math.min(10, 11, 12, 13, 8)); // to find the minimum value
+console.info(Math.round(10.5)); // to round the value to nearest integer
+console.info(Math.round(10.3)); // to round the value to nearest integer
+console.info(Math.floor(10.8)); // to round the value to lower integer
+console.info(Math.ceil(10.3)); // to round the value to upper integer
