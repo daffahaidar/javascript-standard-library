4557a13303456f716480f220f70e4efa3fd75a26
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-08-16 17:48:22 +0700

    Eval

---
+++ 18. Eval/eval.js
@@ -0,0 +1,5 @@
+// eval is a built-in function that executes JavaScript code represented as a string.
+// eval not recommended to use because it is dangerous.
+const script = "alert('Anda di heck')";
+
+eval(script);
